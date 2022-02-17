1378a7014625f6176af0c2a0e3a6047352a7733d
Lukács Zsombor <lukacszsombor11@gmail.com>
2022-02-17 15:41:37 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/adatszerkezetek_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/adatszerkezetek_1.dir/main.c.obj"
+  "adatszerkezetek_1.exe"
+  "adatszerkezetek_1.exe.manifest"
+  "adatszerkezetek_1.pdb"
+  "libadatszerkezetek_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/adatszerkezetek_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/adatszerkezetek_1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for adatszerkezetek_1.
+# This may be replaced when dependencies are built.
